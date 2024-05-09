0f519520956bd2aaa7ca09e1ac454cc91f8bc69c
Jacob <jacobmontenegro1999@gmail.com>
2024-05-08 20:56:37 -0500

    removing unused item

--- app/components/star-background/star-worker.worker.js
+++ app/components/star-background/star-worker.worker.js
@@ -1,6 +1,6 @@
 self.onmessage = function(event) {
     const { stars, mousePosition, windowDimensions } = event.data;
-    const starElements = stars.map((star,index) => {
+    const starElements = stars.map(star => {
         const starPosition = { x: windowDimensions.width * star.left / 100, y: windowDimensions.height * star.top / 100 };
         const distanceSquared = mousePosition.x && mousePosition.y ? Math.pow(starPosition.y - mousePosition.y, 2) + Math.pow(starPosition.x - mousePosition.x, 2) : 1;
         const scale = distanceSquared > 0 ? Math.min(3, 5000 / distanceSquared + 1) :  (1 + Math.random());
